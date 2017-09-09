e01318a2a98adcd40bc91690fb58bf541c02b017
Busayomi Oyewole <busayomioyewole@gmail.com>
2017-09-09 21:32:51 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var workLife = document.getElementById("work");
+
+console.log("This is an element of: ", work.nodeType);
+console.log("The inner HTML is: ", work.innerHTML);
+console.log("child nodes: ", work.childNodes.length);
+
+var myLinks = document.getElementsByTagName("a");
+console.log("Links: ", myLinks.length);
